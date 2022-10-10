2bdecc46c8f180b6cfda0a4d8643297a11efdc47
Nikolarchitect <nikolarchitect@gmail.com>
2022-10-10 23:51:35 +0500

    initialization

---
+++ README.md
@@ -0,0 +1,9 @@
+# requests-to-rest-api
+First steps in network requests
+
+@Nikolarchitect 2022 (nikolarchitect@gmail.com)
+
+#UITableView
+#UISearchBar
+#REST API
+#JSON
---
+++ requests-to-rest-api/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  requests-to-rest-api
+//
+//  Created by Nikolay Kiyko on 10.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupViews()
+    }
+
+    private func setupViews() {
+        view.backgroundColor = .systemGray4
+    }
+}
+
